03433ed39c76c3b19345e3cf00511022480bf0e7
JHL <osjin6633@gmail.com>
2019-10-13 22:46:17 +0900

    LJH

--- stage1.py
+++ stage1.py
@@ -10,12 +10,16 @@ def handle_events():
     global running
     global dir
     global stance_flag
+    global y_dir
+    global jump_count
+    global y
 
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 stance_flag = 1
@@ -23,8 +27,21 @@ def handle_events():
             elif event.key == SDLK_LEFT:
                 stance_flag = 0
                 dir -= 1
+            elif event.key == SDLK_UP:
+                if jump_count <= 0:
+                    y_dir = 1
+                    jump_count += 1
+                elif jump_count >= 2:
+                    while y == 275:
+                        y_dir = 0
+                        y -= 1
+                        if y == 275:
+                            y_dir += 1
+                if y == 275:
+                    jump_count = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
+
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 stance_flag = 3
@@ -35,26 +52,32 @@ def handle_events():
 
 
 x = 400
+y = 275
 frame = 0
 running = True
 dir = 0
 stance_flag = 3
+y_dir = 0
+jump_count = 0
 
 while running:
     clear_canvas()
     stage1.draw(WEIGH / 2, HEIGHT / 2)
-    character.clip_draw(frame*100, stance_flag*100, 100, 100, x, 275)
+    character.clip_draw(frame*100, stance_flag*100, 100, 100, x, y)
     handle_events()
     update_canvas()
     handle_events()
     x += dir*13
+    y += y_dir*6
     frame = (frame + 1) % 8
     if x >= 1280:
-        x = 1280
-        stance_flag = 3
-    elif x <= 0:
         x = 0
-        stance_flag = 2
+    elif x <= 0:
+        x = 1280
+    if y <= 275:
+        y_dir = 0
+    elif y >= 350:
+        y_dir -= 1
     delay(0.04)
 
 
